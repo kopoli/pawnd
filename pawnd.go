19971da656f38a8916e567ac04f7d31d289baad6
Kalle Kankare <kalle.kankare@iki.fi>
2017-09-29 23:31:06 +0300

    pawnd: refactor versioning and error hooking

--- pawnd.go
+++ pawnd.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"strings"
 
 	"github.com/kopoli/go-util"
 	"github.com/kopoli/pawnd/lib"
+)
+
+var (
+	majorVersion     = "0"
+	version          = "Undefined"
+	timestamp        = "Undefined"
+	progVersion      = majorVersion + "-" + version
+	exitValue    int = 0
 )
 
 func printErr(err error, message string, arg ...string) {
@@ -17,28 +26,41 @@ func printErr(err error, message string, arg ...string) {
 	fmt.Fprintf(os.Stderr, "Error: %s%s.%s\n", message, strings.Join(arg, " "), msg)
 }
 
-func checkFault(err error, message string, arg ...string) {
-	if err != nil {
-		printErr(err, message, arg...)
-		os.Exit(1)
-	}
+func fault(err error, message string, arg ...string) {
+	printErr(err, message, arg...)
+
+	// Exit goroutine and run all deferrals
+	exitValue = 1
+	runtime.Goexit()
 }
 
 func main() {
-	opts := util.GetOptions()
+	opts := util.NewOptions()
+
+	opts.Set("program-name", os.Args[0])
+	opts.Set("program-version", progVersion)
+	opts.Set("program-timestamp", timestamp)
 
 	opts.Set("configuration-file", "pawnd.conf")
 
+	// In the last deferred function, exit the program with given code
+	defer func() {
+		os.Exit(exitValue)
+	}()
+
 	_, err := pawnd.Cli(opts, os.Args)
-	checkFault(err, "command line parsing failed")
+	if err != nil {
+		fault(err, "command line parsing failed")
+	}
 
 	if opts.IsSet("demo-mode") {
 		pawnd.UiDemo(opts)
-		os.Exit(25)
+		exitValue = 25
+		return
 	}
 
 	err = pawnd.TestRun(opts)
-	checkFault(err, "Running chains failed")
-
-	os.Exit(0)
+	if err != nil {
+		fault(err, "Running chains failed")
+	}
 }
