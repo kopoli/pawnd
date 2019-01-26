6120dd27e7751a1eee5fbb92f57e84426563fcea
Kalle Kankare <kalle.kankare@iki.fi>
2019-01-26 19:13:23 +0200

    bus: Add restart message and behaviour

--- lib/bus.go
+++ lib/bus.go
@@ -1,9 +1,10 @@
 package pawnd
 
 import (
+	"fmt"
 	"sync"
 
-	"github.com/ryanuber/go-glob"
+	glob "github.com/ryanuber/go-glob"
 )
 
 /*
@@ -21,17 +22,20 @@ Use-cases:
 var (
 	MsgInit = "init"      // Start everything
 	MsgTerm = "terminate" // Stop all processing
+	MsgRest = "restart"   // Restart
 	MsgTrig = "trigger"   // Make action happen
 
-	ToAll = "*"
+	ToAll            = "*"
+	ErrMainRestarted = fmt.Errorf("Main restarted")
 )
 
 // EventBus conveys messages to Listeners
 type EventBus struct {
-	links   map[string]BusLink
-	msgchan chan Message
-	mutex   sync.Mutex
-	wg      sync.WaitGroup
+	links      map[string]BusLink
+	msgchan    chan Message
+	mutex      sync.Mutex
+	wg         sync.WaitGroup
+	restarting bool
 }
 
 // BusLink is the interface for sending messages to the bus
@@ -67,17 +71,26 @@ type Message struct {
 }
 
 func (eb *EventBus) Send(from, to, message string) {
+	if message == MsgRest {
+		eb.restarting = true
+		message = MsgTerm
+	}
 	eb.msgchan <- Message{from, to, message}
 }
 
 // Run until terminated
-func (eb *EventBus) Run() {
+func (eb *EventBus) Run() error {
 	for k := range eb.links {
 		eb.links[k].Run()
 	}
 
 	eb.Send("", ToAll, MsgInit)
 	eb.wg.Wait()
+
+	if eb.restarting {
+		return ErrMainRestarted
+	}
+	return nil
 }
 
 // Action notifies that it has stopped. This is for waiting for all actions
